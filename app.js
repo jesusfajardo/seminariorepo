096e23582b3cd055fb78739ffdc65ef7369b1c15
Jesus <fajardosullcajesus@gmail.com>
2020-09-27 23:06:12 -0400

    rep inicial

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +39,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+var port = 8000;
+app.listen(port, ()=>{
+  console.log("Corriendo " + port);
+});
+
 
 module.exports = app;
